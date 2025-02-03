eac11ab27cc657985a7410deb9e4d0c12a5bf6af
sawongam <sangamadhikari.61@gmail.com>
2025-02-03 09:33:25 +0545

    feat: version 0.1.0 updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,17 @@
+## 0.1.0
+
+### New Features
+- Added a reset timer to automatically reset the tap counter
+- Added optional haptic feedback option
+- Improved tap detection
+
+### Improvements
+- Refactored code for better readability and maintainability.
+- Added validation to ensure `taps` is greater than 0.
+
+### Bug Fixes
+- Fixed potential issues with tap counter reset logic.
+
 ## 0.0.2
 
 * Typo Fixes
--- lib/multi_tap_action.dart
+++ lib/multi_tap_action.dart
@@ -1,38 +1,77 @@
 library multi_tap_action;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MultiTapAction extends StatefulWidget {
+  /// Number of taps required to execute the action
   final int taps;
-  final VoidCallback action;
+
+  /// Callback function to execute when tap count is reached. It will return the number of taps needed to trigger the action.
+  final Function(int taps) onActionTriggered;
+
+  /// Child widget to apply tap detection to
   final Widget child;
 
-  const MultiTapAction(
-      {required this.taps,
-      required this.action,
-      required this.child,
-      super.key});
+  /// Time window to count taps within
+  final Duration resetDuration;
+
+  /// Enable haptic feedback on action trigger
+  final bool enableHapticFeedback;
+
+  const MultiTapAction({
+    super.key,
+    required this.taps,
+    required this.onActionTriggered,
+    required this.child,
+    this.resetDuration = const Duration(minutes: 1),
+    this.enableHapticFeedback = false,
+  }) : assert(taps > 0, 'tapsRequired must be greater than 0');
 
   @override
   State<MultiTapAction> createState() => _MultiTapActionState();
 }
 
 class _MultiTapActionState extends State<MultiTapAction> {
-  int _counter = 0;
+  int _tapCounter = 0;
+  DateTime? _lastTapTime;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          _counter++;
-          if (_counter == widget.taps) {
-            widget.action();
-            _counter = 0;
-          }
-        });
-      },
+      onTap: _handleTap,
+      behavior: HitTestBehavior.opaque,
       child: widget.child,
     );
+  }
+
+  void _handleTap() {
+    final now = DateTime.now();
+
+    if (_lastTapTime != null &&
+        now.difference(_lastTapTime!) > widget.resetDuration) {
+      setState(() {
+        _tapCounter = 0;
+      });
+    }
+
+    setState(() {
+      _tapCounter++;
+      _lastTapTime = now;
+    });
+
+    if (_tapCounter == widget.taps) {
+      widget.onActionTriggered(_tapCounter);
+      if (widget.enableHapticFeedback) {
+        _triggerHapticFeedback();
+      }
+      setState(() {
+        _tapCounter = 0;
+      });
+    }
+  }
+
+  void _triggerHapticFeedback() {
+    HapticFeedback.lightImpact();
   }
 }
